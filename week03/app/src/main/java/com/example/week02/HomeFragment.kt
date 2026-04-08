385093162dc947ed58045f91b11203f4a7c1f8d0
Yeeun102 <164315574+Yeeun102@users.noreply.github.com>
2026-04-09 02:24:37 +0900

    유키/유예은 3주차 미션 제출

--- week03/app/src/main/java/com/example/week02/HomeFragment.kt
+++ week03/app/src/main/java/com/example/week02/HomeFragment.kt
@@ -1,59 +1,46 @@
 package com.example.week02
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager // 🚨 GridLayoutManager 사용을 위해 import 추가 필수!
+import com.example.week02.databinding.FragmentHomeBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [HomeFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class HomeFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_home, container, false)
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
+
+        val dummyList = arrayListOf(
+            Product(R.drawable.ic_launcher_background, "Air Jordan 1", "$150"),
+            Product(R.drawable.ic_launcher_background, "Nike Dunk Low", "$110"),
+            Product(R.drawable.ic_launcher_background, "Air Force 1 '07", "$100"),
+            Product(R.drawable.ic_launcher_background, "Zoom Pegasus 38", "$130"),
+            Product(R.drawable.ic_launcher_background, "Air Jordan 1", "$150"),
+            Product(R.drawable.ic_launcher_background, "Nike Dunk Low", "$110"),
+            Product(R.drawable.ic_launcher_background, "Air Force 1 '07", "$100"),
+            Product(R.drawable.ic_launcher_background, "Zoom Pegasus 38", "$130")
+        )
+
+        val productAdapter = ProductAdapter(dummyList)
+
+        binding.homeProductRv.adapter = productAdapter
+
+        binding.homeProductRv.layoutManager = GridLayoutManager(context, 2)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment HomeFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            HomeFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
--- week03/app/src/main/java/com/example/week02/WishlistFragment.kt
+++ week03/app/src/main/java/com/example/week02/WishlistFragment.kt
@@ -1,59 +1,43 @@
 package com.example.week02
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.GridLayoutManager
+import com.example.week02.databinding.FragmentWishlistBinding
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
-
-/**
- * A simple [Fragment] subclass.
- * Use the [WishlistFragment.newInstance] factory method to
- * create an instance of this fragment.
- */
 class WishlistFragment : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
+    private var _binding: FragmentWishlistBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_wishlist, container, false)
+        _binding = FragmentWishlistBinding.inflate(inflater, container, false)
+
+        val wishlistData = arrayListOf(
+            Product(R.drawable.ic_launcher_background, "조던 1 레트로 하이", "219,000원"),
+            Product(R.drawable.ic_launcher_background, "나이키 덩크 로우", "129,000원"),
+            Product(R.drawable.ic_launcher_background, "에어포스 1 '07", "139,000원"),
+            Product(R.drawable.ic_launcher_background, "줌 보메로 5", "189,000원"),
+            Product(R.drawable.ic_launcher_background, "에어 맥스 97", "199,000원"),
+            Product(R.drawable.ic_launcher_background, "나이키 코르테즈", "99,000원")
+        )
+
+        val wishAdapter = ProductAdapter(wishlistData)
+        binding.wishlistProductRv.adapter = wishAdapter
+
+        binding.wishlistProductRv.layoutManager = GridLayoutManager(context, 2)
+
+        return binding.root
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment WishlistFragment.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            WishlistFragment().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
     }
 }
